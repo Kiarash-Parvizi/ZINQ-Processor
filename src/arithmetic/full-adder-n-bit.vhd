23aa28805df074f8ffbfe8559a5dc5b3adbfd684
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-20 00:11:19 +0430

    Remove some redundant parantheses

--- src/arithmetic/full-adder-n-bit.vhd
+++ src/arithmetic/full-adder-n-bit.vhd
@@ -30,21 +30,21 @@ begin
     l_bit_wise_adders:
     for i in 0 to n - 1 generate
         l_adder_one_bit_first:
-        if (i = 0) generate
+        if i = 0 generate
             adder_first: full_adder_1_bit port map(
                 a(i), b(i), cin, sum(i), carry(i)
             );
         end generate;
 
         l_adders_one_bit_middle:
-        if ((i /= n - 1) and (i /= 0)) generate
+        if (i /= n - 1) and (i /= 0) generate
             adder_middle: full_adder_1_bit port map(
                 a(i), b(i), carry(i - 1), sum(i), carry(i)
             );
         end generate;
 
         l_adder_one_bit_last:
-        if (i = n - 1) generate
+        if i = n - 1 generate
             adder_last: full_adder_1_bit port map(
                 a(i), b(i), carry(i - 1), sum(i), cout
             );
