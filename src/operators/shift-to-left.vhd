9c5b35e93cc545148434c22665064deb9b1f59a5
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-19 17:44:19 +0430

    Add shift_to_left entity

---
+++ src/operators/shift-to-left.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity shift_to_left is
+    generic(
+        len: natural;
+        shift_amount: natural := 1
+    );
+    port(
+        num: in std_logic_vector(len - 1 downto 0);
+        result: out std_logic_vector(len - 1 downto 0)
+    );
+end entity;
+
+architecture structural of shift_to_left is
+begin
+    -- Make right-most bits zero
+    result(shift_amount - 1 downto 0) <= (others => '0');
+
+    -- Make the shifted bits
+    result(len - 1 downto shift_amount) <= num(len - shift_amount - 1 downto 0);
+end architecture;
