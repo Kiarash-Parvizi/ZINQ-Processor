605dbd020c4a15d44efe7691ac1175f3932d3da0
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-23 01:33:16 +0430

    Change the definition of multiplexer_2_to_1 to match VHDL 2002
    Change input from array of std_logic_vectors, into a plain std_logic_
    vector. The convension is explained drastically in the source file.
    
    Also, remove types.vhd useless types.

--- src/utils/types.vhd
+++ src/utils/types.vhd
@@ -1,8 +1,3 @@
-library ieee;
-use ieee.std_logic_1164.all;
-
 package types is
     type time_array is array(natural range <>) of time;
-    type std_logic_vector_array is array(natural range <>) of std_logic_vector;
-    type std_logic_vector_2d_array is array(natural range <>, natural range <>) of std_logic_vector;
 end package;
--- tests/unit/multiplexers/test-multiplexer-2-to-1.vhd
+++ tests/unit/multiplexers/test-multiplexer-2-to-1.vhd
@@ -1,6 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use work.types.std_logic_vector_array;
 
 entity test_multiplexer_2_to_1 is
 end entity;
@@ -11,7 +10,7 @@ architecture structural of test_multiplexer_2_to_1 is
             constant inout_len: natural
         );
         port(
-            inputs: in std_logic_vector_array(2 - 1 downto 0)(inout_len - 1 downto 0);
+            inputs_concat: in std_logic_vector(2 * inout_len - 1 downto 0);
             selector: in std_logic;
             result: out std_logic_vector(inout_len - 1 downto 0)
         );
@@ -19,15 +18,16 @@ architecture structural of test_multiplexer_2_to_1 is
 
     constant inout_len: natural := 4;
 
-    signal result: std_logic_vector_array(2 - 1 downto 0)(inout_len - 1 downto 0);
+    signal result_0, result_1: std_logic_vector(inout_len - 1 downto 0);
 
     signal dummy: bit;
 begin
+    -- Using two writing formats to indicate both are possible and error-free
     instance_selector_0: multiplexer_2_to_1 generic map(inout_len) port map(
-        (b"1100", b"0001"), '0', result(0)
+        (b"1100", b"0001"), '0', result_0
     );
     instance_selector_1: multiplexer_2_to_1 generic map(inout_len) port map(
-        (b"0010", b"0011"), '1', result(1)
+        b"0010" & b"0011", '1', result_1
     );
 
     dummy <= '0' after 10 ns;
