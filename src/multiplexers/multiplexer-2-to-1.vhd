83d9228f73a931dc5bfc9a91bebf322f1d6d4e20
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2021-07-22 19:19:20 +0430

    Add multiplexer_2_to_1 entity and std_logic_vector_array type
    Update Parvaj to use VHDL 2008 by default.

---
+++ src/multiplexers/multiplexer-2-to-1.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.types.std_logic_vector_array;
+
+entity multiplexer_2_to_1 is
+    generic(
+        constant bit_len: natural
+    );
+    port(
+        inputs: in std_logic_vector_array(2 - 1 downto 0)(bit_len - 1 downto 0);
+        selector: in std_logic;
+        result: out std_logic_vector(bit_len - 1 downto 0)
+    );
+end entity;
+
+architecture structural of multiplexer_2_to_1 is
+    signal selector_extended: std_logic_vector(bit_len - 1 downto 0) := (others => selector);
+begin
+    result <=
+        (inputs(0) and not selector_extended) or
+        (inputs(1) and selector_extended);
+end architecture;
--- src/utils/types.vhd
+++ src/utils/types.vhd
@@ -1,3 +1,7 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 package types is
     type time_array is array(natural range <>) of time;
+    type std_logic_vector_array is array(natural range <>) of std_logic_vector;
 end package;
