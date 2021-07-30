bccd18ebcbc0bfd56e78b0f6188e9a0efd4a5c4c
Kiarash-Parvizi <cdev.developer@gmail.com>
2021-07-30 13:27:50 +0430

    replaced tabs with spaces in add-sub-n-bit

--- src/arithmetic/add-sub-n-bit.vhd
+++ src/arithmetic/add-sub-n-bit.vhd
@@ -2,36 +2,36 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity add_sub_n_bit is
-	generic(N: integer);
-	port(
-		a : in std_logic_vector(N-1 downto 0);
-		b : in std_logic_vector(N-1 downto 0);
+    generic(N: integer);
+    port(
+        a : in std_logic_vector(N-1 downto 0);
+        b : in std_logic_vector(N-1 downto 0);
         is_sub: in std_logic := '0';
-		z : out std_logic_vector(N-1 downto 0);
-		cout : out std_logic;
+        z : out std_logic_vector(N-1 downto 0);
+        cout : out std_logic;
         borrow: out std_logic
-	);
+    );
 end entity;
 
 architecture structural of add_sub_n_bit is
-	signal carry: std_logic_vector(N downto 0) := (others => '0');
-	signal bm: std_logic_vector(N-1 downto 0);
-	signal axb: std_logic_vector(N-1 downto 0);
+    signal carry: std_logic_vector(N downto 0) := (others => '0');
+    signal bm: std_logic_vector(N-1 downto 0);
+    signal axb: std_logic_vector(N-1 downto 0);
     signal z_sign: std_logic;
 begin
     z_sign <= carry(N-1) xor axb(N-1);
     carry(0) <= is_sub;
-	cout <= carry(N);
+    cout <= carry(N);
     borrow <= (not a(N-1) and z_sign and not b(N-1)) or
               (a(N-1) and z_sign and b(N-1)) or
               (a(N-1) and not b(N-1));
 
-	-- generator func:
-	g0: for i in N downto 1 generate
-			bm(i-1)  <= b(i-1) xor carry(0);
-			axb(i-1) <= a(i-1) xor bm(i-1);
-			z(i-1) <= carry(i-1) xor axb(i-1);
-			carry(i) <= (a(i-1) and bm(i-1)) or
-						(axb(i-1) and carry(i-1));
-	end generate g0;
+    -- generator func:
+    g0: for i in N downto 1 generate
+            bm(i-1)  <= b(i-1) xor carry(0);
+            axb(i-1) <= a(i-1) xor bm(i-1);
+            z(i-1) <= carry(i-1) xor axb(i-1);
+            carry(i) <= (a(i-1) and bm(i-1)) or
+                        (axb(i-1) and carry(i-1));
+    end generate g0;
 end architecture;
--- tests/unit/arithmetic/test-add-sub-n-bit.vhd
+++ tests/unit/arithmetic/test-add-sub-n-bit.vhd
@@ -6,46 +6,46 @@ end entity;
 
 architecture structural of test_add_sub_n_bit is
     component add_sub_n_bit is
-    	generic(N: integer);
-    	port(
-    		a : in std_logic_vector(N-1 downto 0);
-    		b : in std_logic_vector(N-1 downto 0);
+        generic(N: integer);
+        port(
+            a : in std_logic_vector(N-1 downto 0);
+            b : in std_logic_vector(N-1 downto 0);
             is_sub: in std_logic := '0';
-    		z : out std_logic_vector(N-1 downto 0);
-    		cout : out std_logic;
+            z : out std_logic_vector(N-1 downto 0);
+            cout : out std_logic;
             borrow: out std_logic
-    	);
+        );
     end component;
-	-- helper vars
-	constant N: integer := 4;
-	signal a : std_logic_vector(N-1 downto 0);
-	signal b : std_logic_vector(N-1 downto 0);
-	signal z : std_logic_vector(N-1 downto 0);
-	signal cout, borrow : std_logic;
+    -- helper vars
+    constant N: integer := 4;
+    signal a : std_logic_vector(N-1 downto 0);
+    signal b : std_logic_vector(N-1 downto 0);
+    signal z : std_logic_vector(N-1 downto 0);
+    signal cout, borrow : std_logic;
     signal is_sub : std_logic := '1';
-	--
+    --
 begin
     uut0: add_sub_n_bit generic map(N)
         port map(a, b, is_sub, z, cout, borrow);
-	test0: process is begin
-		a <= "1101"; b <= "0101";
-		wait for 10 ns;
-		a <= "1111"; b <= "1101";
-		wait for 10 ns;
-		a <= "1101"; b <= "1111";
-		wait for 10 ns;
-		a <= "0101"; b <= "0101";
-		wait for 10 ns;
-		a <= "0101"; b <= "0111";
-		wait for 10 ns;
-		a <= "0111"; b <= "0101";
-		wait for 10 ns;
-		a <= "0110"; b <= "0110";
-		wait for 10 ns;
-		a <= "0101"; b <= "1111";
-		wait for 10 ns;
-		a <= "0111"; b <= "1110";
-		wait for 10 ns;
-		wait;
-	end process;
+    test0: process is begin
+        a <= "1101"; b <= "0101";
+        wait for 10 ns;
+        a <= "1111"; b <= "1101";
+        wait for 10 ns;
+        a <= "1101"; b <= "1111";
+        wait for 10 ns;
+        a <= "0101"; b <= "0101";
+        wait for 10 ns;
+        a <= "0101"; b <= "0111";
+        wait for 10 ns;
+        a <= "0111"; b <= "0101";
+        wait for 10 ns;
+        a <= "0110"; b <= "0110";
+        wait for 10 ns;
+        a <= "0101"; b <= "1111";
+        wait for 10 ns;
+        a <= "0111"; b <= "1110";
+        wait for 10 ns;
+        wait;
+    end process;
 end architecture;
