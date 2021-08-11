2233f34bf143b84d7b20bd92761adbf6b37a1d3f
Kiarash-Parvizi <cdev.developer@gmail.com>
2021-08-11 21:16:37 +0430

    added report statements for debugging

--- src/storage/ram.vhd
+++ src/storage/ram.vhd
@@ -16,10 +16,21 @@ end entity;
 architecture structural of ram is
     type ram_type is array (0 to (2**address'length)-1) of std_logic_vector(datain'range);
     signal ramBuf : ram_type := (others => (others => '0'));
+    function to_string(inp: std_logic_vector)return string is variable image_str: string (1 to inp'length);
+        alias input_str: std_logic_vector (1 to inp'length) is inp; begin for i in input_str'range loop image_str(i)
+        := character'VALUE(std_ulogic'IMAGE(input_str(i))); end loop; return image_str; end function;
 begin
     RamProc: process(clock) is
     begin
         if rising_edge(clock) then
+            --report "ram: " & integer'image(address'length);
+            if (address'length = 16) then
+                report "ram.addr<0x0000, 0xFFFF, 0x3FFF> = <" &
+                    to_string(ramBuf(16#0000#)) & ", " &
+                    to_string(ramBuf(16#ffff#)) & ", " &
+                    to_string(ramBuf(16#3fff#)) & ", " &
+                ">\n";
+            end if;
             if we = '1' then
                 ramBuf(to_integer(unsigned(address))) <= datain;
             end if;
--- src/storage/register-file-3.vhd
+++ src/storage/register-file-3.vhd
@@ -26,6 +26,9 @@ end entity;
 architecture structural of register_file_3 is
     type rf_type is array (0 to (2**addr_size)-1) of std_logic_vector(wd'range);
     signal rfBuf : rf_type := (others => (others => '0'));
+    function to_string(inp: std_logic_vector)return string is variable image_str: string (1 to inp'length);
+        alias input_str: std_logic_vector (1 to inp'length) is inp; begin for i in input_str'range loop image_str(i)
+        := character'VALUE(std_ulogic'IMAGE(input_str(i))); end loop; return image_str; end function;
 begin
     out0 <= rfBuf(to_integer(unsigned(r0)));
     out1 <= rfBuf(to_integer(unsigned(r1)));
@@ -33,12 +36,27 @@ begin
     -- process
     process(clk, rst) is begin
         -- reset
+        --report "rf3.process";
         if(rst='1') then
+            report "rf3.if(1)";
             for i in 0 to rfBuf'length-1 loop
                 rfBuf(i) <= (others => '0');
             end loop;
         -- write
         elsif(we='1' and clk'event and clk='1') then
+            report "rf3.if(2)";
+            if (addr_size = 3) then
+                report "rf3.addr<all> = <" &
+                    to_string(rfBuf(0)) & ", " &
+                    to_string(rfBuf(1)) & ", " &
+                    to_string(rfBuf(2)) & ", " &
+                    to_string(rfBuf(3)) & ", " &
+                    to_string(rfBuf(4)) & ", " &
+                    to_string(rfBuf(5)) & ", " &
+                    to_string(rfBuf(6)) & ", " &
+                    to_string(rfBuf(7)) & ", " &
+                ">\n";
+            end if;
             rfBuf(to_integer(unsigned(wr))) <= wd;
         end if;
     end process;
